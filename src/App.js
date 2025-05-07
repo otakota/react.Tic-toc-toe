d7a182d00ef9534a202411f97be4a399ff3fed5c
otakota <136304314+otakota@users.noreply.github.com>
2025-05-07 12:59:50 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+const Square = () => {
+  const [value, setValue] = useState(null);
+  return <button className="square">{value}</button>;
+};
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const handleClick = () => {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  };
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
+    </>
+  );
 }
